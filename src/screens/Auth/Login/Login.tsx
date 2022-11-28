23dc938e48d7b53ef8b967aabd2aaaecc749b3b8
Mihir <mihirkuvardiyait3@gmail.com>
2022-11-28 19:45:35 +0530

    minor change

--- src/screens/Auth/Login/Login.tsx
+++ src/screens/Auth/Login/Login.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 const LoginScreen = () => {
     return(
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <Text style={{color:'red'}}>login screen</Text>
+            <Text style={{color:'red' }}>login screen</Text>
         </View>
     )
 }
