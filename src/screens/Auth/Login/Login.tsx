70a83eb2342373043c786540df7beded0943e5f2
Mihir <mihirkuvardiyait3@gmail.com>
2022-11-28 19:42:30 +0530

    dummy lodin screen created

--- src/screens/Auth/Login/Login.tsx
+++ src/screens/Auth/Login/Login.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 const LoginScreen = () => {
     return(
-        <View>
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
             <Text style={{color:'red'}}>login screen</Text>
         </View>
     )
