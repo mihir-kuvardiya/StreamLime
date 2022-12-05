063d5b60a5f42fe29ca3d62325fa7f5dbe427526
mihir-lnxct <mihirkuvardiyait3@gmail.com>
2022-12-05 21:49:19 +0530

    bottom tab naviagtion icon added

--- src/naviagtion/bottomTabNavigation.tsx
+++ src/naviagtion/bottomTabNavigation.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import UserProfileScreen from "../screens/Feed/profile/userProfile";
 import FeedList from "../screens/Feed/feedList/feedList";
 import CreateFeedScreen from "../screens/Feed/createFeed/createFeed";
 import screenNameEnum from "../helper/screenNameEnum";
-import { ms } from "react-native-size-matters";
+import { ms, ScaledSheet } from "react-native-size-matters";
+import colorPalates from "../theme/colorPalates";
+import IconFeather from 'react-native-vector-icons/Feather';
 
 const BottomTabNavigation = () => {
 
@@ -16,19 +18,51 @@ const BottomTabNavigation = () => {
             route: screenNameEnum.FeedList,
             label: 'FeedList',
             component: FeedList,
+            icon: 'home'
         },
         {
             route: screenNameEnum.CreateFeedScreen,
             label: 'CreateFeedScreen',
             component: CreateFeedScreen,
+            icon: 'plus-square'
         },
         {
             route: screenNameEnum.UserProfileScreen,
             label: 'UserProfileScreen',
             component: UserProfileScreen,
+            icon: 'user'
         }
     ]
     
+    const TabButton = (props: {item: any;onPress: any;accessibilityState: any}) => {
+
+        const {item, onPress, accessibilityState} = props;
+        const focused = accessibilityState.selected;
+
+        return(
+            <TouchableOpacity
+            onPress={onPress}
+            activeOpacity={1}
+            style={styles.container}>
+              <View
+                style={[
+                  focused ? {borderWidth: ms(0)} : {borderWidth: 0},
+                  styles.button,
+                ]}>
+                {/* <SvgXml
+                  xml={!focused ? item?.name : item?.name2}
+                  height={focused ? ms(30) : ms(53)}
+                  width={focused ? ms(30) : ms(53)}
+                /> */}
+                <IconFeather
+                    name={item?.icon}
+                    size={30}
+                    color={focused ? colorPalates.AppTheme.primary : colorPalates.AppTheme.text}
+                />
+              </View>
+          </TouchableOpacity>
+        )
+    }
 
     return(
         <bottomTab.Navigator screenOptions={{headerShown: false}}>
@@ -39,12 +73,14 @@ const BottomTabNavigation = () => {
                         name={item.route} 
                         component={item.component} 
                         options={{
-                            tabBarShowLabel: true,
+                            tabBarShowLabel: false,
                             tabBarStyle:{
                                 position:"absolute",
-                                margin:ms(15),
-                                borderRadius:ms(10)
-                            }
+                                margin:ms(20),
+                                borderRadius:ms(20),
+                                height:ms(50)
+                            },
+                            tabBarButton: props => <TabButton {...props} item={item} />,
                         }}
                     />
                 )
@@ -54,3 +90,19 @@ const BottomTabNavigation = () => {
 }
 
 export default BottomTabNavigation;
+
+const styles = ScaledSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+      width: ms(40),
+      height: ms(40),
+      borderRadius: ms(20),
+      borderColor: colorPalates.white,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
