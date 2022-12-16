439d46291c02cc116d0b7107d53880d9492a6ed5
Mihir <mihirkuvardiyait3@gmail.com>
2022-12-16 18:12:52 +0530

    back to previous screen and user profile screen added in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Image, SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import colorPalates from "../../theme/colorPalates";
 import headerStyle from "./headerStyle";
 import IconFeather from 'react-native-vector-icons/Feather';
+import { useNavigation } from "@react-navigation/native";
+import screenNameEnum from "../../helper/screenNameEnum";
 
 export interface HeaderProps {
     title?: string,
@@ -11,22 +13,35 @@ export interface HeaderProps {
 }
 
 const Header = ({title='Header', isBack=false, isProfileSave=false}:HeaderProps) => {
+    
+    const naviagtion = useNavigation();
+
+    const onPressProfile = () => {
+        naviagtion.navigate(screenNameEnum.UserProfileScreen);
+    }
+
+    const onPressBack = () => {
+        naviagtion.goBack();
+    }
+    
     return(
         <SafeAreaView style={headerStyle.mainContainer}>
             <View style={headerStyle.container}>
                 {
                     isBack ?
-                    <View style={headerStyle.iconContainer}>
+                    <TouchableOpacity style={headerStyle.iconContainer} onPress={onPressBack}>
                         <IconFeather name='chevron-left' color={colorPalates.AppTheme.text} size={25}/>
-                    </View>
+                    </TouchableOpacity>
                     :
-                    <Image
-                        style={headerStyle.profileImage}
-                        source={{
-                            uri: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600',
-                        }}
-                        resizeMode={"cover"} 
-                    />
+                    <TouchableOpacity onPress={onPressProfile}>
+                        <Image
+                            style={headerStyle.profileImage}
+                            source={{
+                                uri: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600',
+                            }}
+                            resizeMode={"cover"} 
+                        />
+                    </TouchableOpacity>
                 }
                 <Text style={headerStyle.headerTitle}>{title}</Text>
                 {!isProfileSave ? <View style={headerStyle.emptyView}/> :
