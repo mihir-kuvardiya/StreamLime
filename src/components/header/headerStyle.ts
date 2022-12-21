8372d3c20825cab29041af052236a8ea9be66715
Mihir <mihirkuvardiyait3@gmail.com>
2022-12-21 19:00:13 +0530

    header style hieght fixed

--- src/components/header/headerStyle.ts
+++ src/components/header/headerStyle.ts
@@ -10,7 +10,7 @@ export default ScaledSheet.create({
         justifyContent:'space-between',
         alignItems:'center',
         paddingHorizontal:ms(15),
-        paddingVertical: ms(4),
+        paddingVertical: ms(6),
         // borderBottomLeftRadius: ms(20),
         // borderBottomRightRadius: ms(20),
         borderRadius:ms(10),
