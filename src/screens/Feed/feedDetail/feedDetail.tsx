6cb6e171b7668ea6e48dc8cd221ff7989c46634e
mihir-lnxct <mihirkuvardiyait3@gmail.com>
2023-01-23 22:33:09 +0530

    loading indicator added in feed detail

--- src/screens/Feed/feedDetail/feedDetail.tsx
+++ src/screens/Feed/feedDetail/feedDetail.tsx
@@ -1,27 +1,30 @@
 import { useRoute } from "@react-navigation/native";
-import React, { useEffect } from "react"
-import { SafeAreaView } from "react-native"
+import React, { useEffect, useState } from "react"
+import { ActivityIndicator, SafeAreaView, View } from "react-native"
 import Header from "../../../components/header/header";
 import FeedCard from "../feedList/FeedCard/feedCard";
 import firestore from '@react-native-firebase/firestore';
 import { useDispatch } from "react-redux";
 import { feedAction, useFeedDetailData } from "../../../redux/reducers/feedSlice/feedSlice";
+import colorPalates from "../../../theme/colorPalates";
 
 const FeedDetailScreen = () => {
 
-    const route = useRoute();
+    const route:any = useRoute();
     const dispatch = useDispatch();
     const feedDetail = useFeedDetailData();
+    const [loading, setLoading] = useState(false);
 
     useEffect(()=>{
         getUniquePost();
     },[])
     
     const getUniquePost = async () => {
+        setLoading(true);
         try {
-            const postData = await firestore().collection('posts').doc(route?.params?.postId).get();
+            const postData:any = await firestore().collection('posts').doc(route?.params?.postId).get();
             let date = new Date(postData._data?.createdAt.toDate())
-            const userDetail = await firestore().collection('user').doc(postData._data.userId).get();
+            const userDetail:any = await firestore().collection('user').doc(postData._data.userId).get();
             dispatch(feedAction.setFeedData({
                 collectionName:'feedDetail',
                 data:{
@@ -35,20 +38,25 @@ const FeedDetailScreen = () => {
                     userName:userDetail._data?.userName,
                 }
             }))
-           
+           setLoading(false);
         } catch (error) {
             console.log(error,'error from get unique post')
+            setLoading(false);
         }
     }
 
     return(
         <>
-        <SafeAreaView>
+        <SafeAreaView style={{flex:1}}>
             <Header isBack={true} title={'StreamLine'} />
+            {loading ?
+                <ActivityIndicator size={'large'} color={colorPalates.AppTheme.primary} style={{flex:1,justifyContent:'center',alignItems:'center'}}/>
+                :
+                <FeedCard 
+                    item={feedDetail}
+                />
+            }
         </SafeAreaView>
-        <FeedCard 
-            item={feedDetail}
-        />
         </>
     )
 }
